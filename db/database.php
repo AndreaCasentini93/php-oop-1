9d4a680e0378ee6d37a5c4d1502a7e34fcf974bb
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-07-08 15:52:34 +0200

    Database

--- db/database.php
+++ db/database.php
@@ -16,13 +16,39 @@
             "title" => "Il padrino",
             "original_title" => "The Godfather",
             "original_language" => "en",
-            "vote" => 4,
+            "vote" => 5,
             "genre" => "drama",
             "year_release" => "21-09-1972",
             "plot" => "Il film narra la storia di Marty McFly (Micheal J. Fox), un adolescente californiano alle prese con uno scienziato carismatico e un po’ strampalato, Emmett “Doc” Brown (Christopher Lloyd). Doc invita Marty a raggiungerlo al parcheggio di un grande centro commerciale per aiutarlo a filmare un esperimento. Una volta giunto lì, Marty scopre che l’esperimento è in realtà una macchina del tempo, costruita all’interno di una DeLorean modificata. Lo scienziato rivela che la macchina è alimentata a plutonio e confessa di aver rubato il prezioso elemento ai terroristi libici.
             Nel tentativo di spiegare a Marty il funzionamento della macchina, Doc seleziona il 5 Novembre 1955 come data di viaggio. Proprio in quel momento, sopraggiungono inaspettatamente i terroristi, i quali sparano allo scienziato. Marty si rifugia dentro la DeLorean e inavvertitamente ne attiva i comandi, e così viaggia indietro nel tempo, fino al 1955. Qui il ragazzo scopre di non avere plutonio a sufficienza per tornare indietro. Così si mette alla ricerca del giovane Doc, l’unico che possa aiutarlo.
             Strada facendo incontra George (Crispin Glover), suo futuro padre, bullizzato dal compagno di classe Biff Tannen (Thomas F. Wilson), e, nel seguirlo, finisce con l'intromettersi nel suo passato. Infatti, Lorraine (Lea Thompson) si invaghisce di Marty, innescando un paradosso temporale pericolosissimo, dal momento che la ragazza è la sua futura madre.
             Una volta rintracciato Doc, lo scienziato si mette all’opera per escogitare un piano per riportare Marty “indietro” nel futuro e mette in guardia il ragazzo: dovrà necessariamente fare in modo che i suoi genitori si innamorino quella notte stessa, o verrà cancellato dalla faccia della terra."
+        ],
+        [
+            "title" => "Fight Club",
+            "original_title" => "Fight Club",
+            "original_language" => "en",
+            "vote" => 3,
+            "genre" => "thriller",
+            "year_release" => "29-19-1999",
+            "plot" => "Il narratore e protagonista della storia (Edward Norton), la cui identità non viene mai rivelata, è un consulente assicurativo per conto di una grande casa automobilistica. Insoddisfatto del lavoro, depresso, insonne, ansioso, egli sembra trovare conforto solo fingendo di essere affetto da una moltitudine di malattie terminali e partecipando agli incontri dei relativi gruppi di supporto. Durante uno di questi incontri, conosce Marla Singer (Helena Bonham Carter), la quale, come il protagonista, frequenta gli incontri pur godendo di buona salute. I due decidono, per quieto vivere, di spartirsi i gruppi ai quali partecipare.
+            Al ritorno da un viaggio di lavoro, il narratore incontra Tyler Durden (Brad Pitt), un eccentrico venditore di saponette. Quello stesso giorno, giunto a casa, il narratore scopre che il suo appartamento è stato distrutto da un’esplosione causata da una perdita di gas. Disperato, chiama Tyler in cerca di aiuto e i due si incontrano in un bar. Tyler acconsente a ospitare il protagonista nella sua abitazione fatiscente, e tra i due nasce un bizzarro rapporto, fatto di discorsi sovversivi e violenti combattimenti.
+            Questa amicizia-simbiosi viene consolidata infine dalla creazione del “Fight Club”, un circolo segreto in cui i membri prendono parte a combattimenti brutali a scopo ricreativo. In poco tempo, il Fight Club diventa un covo di adulti insoddisfatti della vita e della società in cui vivono e la cui unica valvola di sfogo è l’alienazione nella violenza. Il club raduna adepti da tutta la nazione, e nasce il Progetto Mayhem, di stampo anticonsumistico e sovversivo, guidato dalle idee sempre più scellerate di Tyler. Quando il narratore, confuso e preoccupato dalla pericolosità del progetto, tenta di discuterne con Tyler, questi scompare; così il narratore si mette alla ricerca di Tyler, fino a scoprire la sua vera identità."
+        ],
+        [
+            "title" => "Lo squalo",
+            "original_title" => "Jaws",
+            "original_language" => "en",
+            "vote" => 4,
+            "genre" => "horror",
+            "year_release" => "19-12-1975",
+            "plot" => "Nei giorni che precedono la Festa del 4 luglio, la giovane Christine Watkins, scompare dopo essersi tuffata in mare per un bagno di mezzanotte. Il giorno seguente viene rinvenuto sulla battigia il corpo senza vita della ragazza. Le profonde lacerazioni presenti nel corpo martoriato sono compatibili con un attacco di uno squalo.
+            Il capo della polizia, Martin Brody (Roy Scheider), chiamato ad indagare sul caso, è deciso a chiudere immediatamente la spiaggia per motivi di sicurezza, ma il sindaco Larry Vaughan (Murray Hamilton), si oppone per timore che la notizia della morte della ragazza possa generare un panico collettivo comportando una drastica riduzione del turismo, principale fonte di reddito della città.
+            Vaughan induce anche il medico legale a falsificare gli esiti dell'autopsia pur di non far trapelare la tragica verità. Brody si trova costretto a sottostare alla volontà del sindaco accettando questa nuova versione della morte di Christine.
+            Ma pochi giorni più tardi un altro bagnante, l'adolescente Alex Kintner, muore dilaniato da un pescecane.
+            Si scatena a questo punto una folla caccia allo squalo tra i pescatori e il cacciatore professionista Quint (Robert Shaw). Il ritrovamento di uno squalo tigre, fa credere che l'incubo sia finalmente terminato, ma il biologo marino Matt Hooper (Richard Dreyfuss), ingaggiato da Brody per ricevere aiuto sul caso, è convinto che non sia lui lo squalo feroce responsabile delle due morti. Effettivamente si tratta di un altro pescecane, il pericoloso squalo purtroppo si aggira ancora tra le acque gremite di bagnanti. Nel giro di poco tempo un altro uomo viene ucciso e un ragazzo, Michel, riesce ad evitare l'attacco del feroce pescecane solo perché tratto miracolosamente in salvo, seppur svenuto, da alcuni bagnanti.
+            Il ragazzo salvato è il figlio del poliziotto Brody, che decide a questo punto di mettersi in gioco in prima persona.
+            Il capo della polizia affronterà il mare insieme a Hooper e al supponente Quint, a bordo dell'imbarcazione di quest'ultimo, alla ricerca del pericoloso squalo. Ma l'impresa si rivelerà decisamente più complicata e azzardata del previsto.."
         ]
     ];
 
